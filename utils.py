4978c10416e5e74084d2632e6879fa33e1d4df9b
Yago Boleas <yagobf.ybf@gmail.com>
2025-03-17 00:03:11 +0100

    [new] Added the Adversarial Loss function

--- utils.py
+++ utils.py
@@ -85,18 +85,57 @@ def load_dataset(dataset: str, scaling: str = "none", backend: str = "torch") ->
         raise ValueError("Invalid backend. Choose 'torch' or 'tf'.")
 
     return X_train, y_train, X_test, y_test
+
+def predict_proba(model, data, device='cpu'):
     """
-    Classifier's probability for the desired class y_nun given x'
+    Predicts the class probabilities and the predicted class for the given data using the model.
+
+    The function performs preprocessing on the input data, passes it through the model, and returns the probabilities for each class as well as the predicted class.
+
+    :param `model`: The trained model used for making predictions.
+    :param `data`: The input data (either as a tensor or a numpy array) for which predictions are made.
+    :param `device`: The device ('cpu' or 'cuda') on which the model and data should be processed.
+    :return `probabilities`: A tensor containing class probabilities for the input data
+    _return `predicted_class`: The index of the class with the highest probability
+    """
+    model.eval()
+
+    if isinstance(data, np.ndarray):
+        data_tensor = torch.tensor(data, dtype=torch.float32)
+    else:
+        data_tensor = data
+    if len(data_tensor.shape) == 2:
+        seq_length, features = data_tensor.shape
+        data_tensor = data_tensor.reshape(1, features, seq_length)
+    elif len(data_tensor.shape) == 1:
+        seq_length = data_tensor.shape[0]
+        data_tensor = data_tensor.reshape(1, 1, seq_length)
+
+    data_tensor = data_tensor.to(device)
+    model = model.to(device)
+    
+    with torch.no_grad():
+        logits = model(data_tensor)
+        probabilities = F.softmax(logits, dim=1)
+        _, predicted_class = torch.max(logits, 1)
+    
+    return probabilities, predicted_class.item()
+
+def adversarial_loss(x, y_nun, model, device='cpu'):
+    """
+    Classifier's probability for the desired class y_nun given x'.
 
     :param `x`: New generated sample
     :param `y_nun`: Label of the desired class
-    :return: Probability prediction of the model 
+    :return `prob`: Probability prediction of the model 
     """
-    raise NotImplementedError
+    probabilities, pred_class = predict_proba(model, x, device)
+    prob = probabilities[0, y_nun].item()
+    return prob, pred_class
 
 def l0_norm(mask: np.ndarray) -> int:
     """
-    Calculates the L0 norm of a mask
+    Calculates the L0 norm of a mask.
 
     :param `mask`: Numpy array representing the mask
     :return: Number of non-zero elements in the mask
@@ -105,11 +144,11 @@ def l0_norm(mask: np.ndarray) -> int:
 
 def sparsity_loss(mask: np.ndarray) -> float:
     """
-    Calculates the sparsity loss of a mask
+    Calculates the sparsity loss of a mask.
 
     :param `mask`: Numpy array representing the mask
     :return: Sparsity loss
-    :raises ValueError: If the mask is empty
+    :raises `ValueError`: If the mask is empty
     """
     if len(mask) == 0:
         raise ValueError("The mask cannot be empty.")
@@ -117,7 +156,7 @@ def sparsity_loss(mask: np.ndarray) -> float:
 
 def num_subsequences(mask: np.ndarray) -> int:
     """
-    Calculates the number of subsequences in a mask
+    Calculates the number of subsequences in a mask.
 
     :param `mask`: Numpy array representing the mask
     :return: Number of subsequences
@@ -126,12 +165,12 @@ def num_subsequences(mask: np.ndarray) -> int:
 
 def contiguity_loss(mask: np.ndarray, gamma: float = 0.25) -> float:
     """
-    Calculates the contiguity loss of a mask
+    Calculates the contiguity loss of a mask.
 
     :param `mask`: Numpy array representing the mask
     :param `gamma`: Adjustment parameter, default is 0.25
     :return: Contiguity loss
-    :raises ValueError: If the mask is empty
+    :raises `ValueError`: If the mask is empty
     """
     if len(mask) == 0:
         raise ValueError("The mask cannot be empty.")
@@ -139,7 +178,7 @@ def contiguity_loss(mask: np.ndarray, gamma: float = 0.25) -> float:
 
 def l1_norm(x: tp.Union[np.ndarray, list], cfe: tp.Union[np.ndarray, list]) -> float:
     """
-    Calculates the L1 norm between two arrays
+    Calculates the L1 norm between two arrays.
 
     :param `x`: First array
     :param `cfe`: Second array
@@ -151,7 +190,7 @@ def l1_norm(x: tp.Union[np.ndarray, list], cfe: tp.Union[np.ndarray, list]) -> f
 
 def l2_norm(x: tp.Union[np.ndarray, list], cfe: tp.Union[np.ndarray, list]) -> float:
     """
-    Calculates the L2 norm between two arrays
+    Calculates the L2 norm between two arrays.
 
     :param `x`: First array
     :param `cfe`: Second array
@@ -168,11 +207,11 @@ def load_model(dataset, experiment, t='best'):
     This function loads a model from the specified dataset and experiment folder. 
     The experiment can be selected based on various criteria: 'best', 'random', 'worst', 'median', or an integer index to choose a specific experiment.
 
-    :param dataset: The name of the dataset
-    :param experiment: The name of the experiment folder within the dataset
-    :param t: The criteria for selecting the experiment. Can be 'best', 'random', 'worst', 'median' or an integer index. Default is 'best'
+    :param `dataset`: The name of the dataset
+    :param `experiment`: The name of the experiment folder within the dataset
+    :param `t`: The criteria for selecting the experiment. Can be 'best', 'random', 'worst', 'median' or an integer index. Default is 'best'
     :return `model`: The trained model loaded from the specified experiment
-    :raises ValueError: If `t` is not one of the valid selection methods or if an invalid index is provided
+    :raises `ValueError`: If 't' is not one of the valid selection methods or if an invalid index is provided
     """
     from pandas import read_excel
     exp_path = os.path.join('models', dataset, experiment)
